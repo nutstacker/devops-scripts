82e159e4eda194a1af950b947dd4501979757240
P J Manu Prathap <62021759+nutstacker@users.noreply.github.com>
2021-07-12 02:50:01 +0530

    Update deploy _war_to_artifactory.sh

--- deploy _war_to_artifactory.sh
+++ deploy _war_to_artifactory.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -u${BAMBOO_USERNAME}:${BAMBOO_PASSWORD} -T "../target/hello-world-war-1.0.0.war" "${BAMBOO_URL_ARTIFACTORY}${BAMBOO_URL_REPO}${BAMBOO_WAR_NAME}.war"
+curl -u${BAMBOO_USERNAME}:${BAMBOO_PASSWORD} -T target/hello-world-war-1.0.0.war "http://localhost:8081/artifactory/example-repo-local/my/jar/1.0/hello-world-war.war"
