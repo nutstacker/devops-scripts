89e7cbe152310f40ee8ed7651ac9260641d9ac0e
P J Manu Prathap <62021759+nutstacker@users.noreply.github.com>
2021-07-12 03:35:22 +0530

    Update artifactory_to_tomcat.sh

--- artifactory_to_tomcat.sh
+++ artifactory_to_tomcat.sh
@@ -3,4 +3,5 @@ sh /root/apache-tomcat-10.0.6/bin/shutdown.sh
 cd /root/apache-tomcat-10.0.6/webapps/
 rm -rf *
 curl -u${BAMBOO_USERNAME}:${BAMBOO_PASSWORD} -O "http://localhost:8081/artifactory/example-repo-local/my/jar/1.0/hello-world-war.war"
+cp hello-world-war.war /root/apache-tomcat-10.0.6/webapps/hello-world-war.war
 sh /root/apache-tomcat-10.0.6/bin/startup.sh
